479a49a96b8bc5f7a5374e71c9f0ffbd1919a7ed
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-03-06 03:39:55 -0600

    Add app file

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
