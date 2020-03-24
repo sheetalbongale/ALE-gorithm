b91505dcf5a42f40420cf15558c6c00bbe5efb61
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-03-24 05:37:11 -0500

    Add schema for the merged master datafile

--- data/db/load_to_mysql.py
+++ data/db/load_to_mysql.py
@@ -12,7 +12,7 @@ USER = "root"
 PASSWORD = config.password
 HOST = "127.0.0.1"
 PORT = "3306"
-DATABASE = "beers_db"
+DATABASE = "alegorithm_db"
 
 engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}")
 
@@ -26,21 +26,35 @@ except ProgrammingError:
 
 engine.execute(f"USE {DATABASE}")
 
-BEERS_TABLENAME1 = "beers"
+BEERS_TABLENAME1 = "beers_and_reviews"
 engine.execute(f"DROP TABLE IF EXISTS {BEERS_TABLENAME1}")
 
-df = pd.read_csv("../csv/clean_beers.csv").to_sql(
+df = pd.read_csv("../csv/final_merged_data.csv").to_sql(
     name=BEERS_TABLENAME1,
     con=engine,
     index=False,
-    dtype = {'id': sqlalchemy.types.String(length=50), 
-    'name': sqlalchemy.types.String(length=300),
-    'brewery_id': sqlalchemy.types.String(length=50),
-    'state': sqlalchemy.types.String(length=50),
-    'country': sqlalchemy.types.String(length=50),
-    'style': sqlalchemy.types.String(length=100),
-    'availability': sqlalchemy.types.String(length=50),
+    dtype = {
+    'beer_id': sqlalchemy.types.String(length=50), 
+    'score': sqlalchemy.types.INTEGER, 
+    'beer_name': sqlalchemy.types.String(length=300), 
+    'brewery_id': sqlalchemy.types.String(length=50), 
+    'state': sqlalchemy.types.String(length=50), 
+    'country': sqlalchemy.types.String(length=10), 
+    'beer_style': sqlalchemy.types.String(length=300), 
+    'availability': sqlalchemy.types.String(length=100), 
     'abv': sqlalchemy.types.INTEGER, 
-    'retired': sqlalchemy.types.String(length=50)
+    'brewery_name': sqlalchemy.types.String(length=100), 
+    'city': sqlalchemy.types.String(length=50), 
+    'types': sqlalchemy.types.String(length=50), 
+    'Category': sqlalchemy.types.String(length=50), 
+    'ABV (min)': sqlalchemy.types.INTEGER, 
+    'ABV (max)': sqlalchemy.types.INTEGER, 
+    'ABV (avg)': sqlalchemy.types.INTEGER, 
+    'IBU (min)': sqlalchemy.types.INTEGER, 
+    'IBU (max)': sqlalchemy.types.INTEGER, 
+    'IBU (avg)': sqlalchemy.types.INTEGER, 
+    'SRM Range': sqlalchemy.types.String(length=50), 
+    'Glassware': sqlalchemy.types.String(length=50), 
+    'Description': sqlalchemy.types.String(length=500)
     })
 
