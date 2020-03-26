b94e23e8c6f89644e33a5d1152f9c31a59d51097
Barry <semieng94@gmail.com>
2020-03-25 20:21:42 -0500

    working on load to mysql

--- data/db/load_to_mysql.py
+++ data/db/load_to_mysql.py
@@ -12,7 +12,7 @@ USER = "root"
 PASSWORD = config.password
 HOST = "127.0.0.1"
 PORT = "3306"
-DATABASE = "alegorithm_db"
+DATABASE = "alegorithm_db2"
 
 engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}")
 
@@ -34,27 +34,26 @@ df = pd.read_csv("../csv/final_merged_data.csv").to_sql(
     con=engine,
     index=False,
     dtype = {
-    'beer_id': sqlalchemy.types.String(length=50), 
-    'score': sqlalchemy.types.INTEGER, 
-    'beer_name': sqlalchemy.types.String(length=300), 
-    'brewery_id': sqlalchemy.types.String(length=50), 
-    'state': sqlalchemy.types.String(length=50), 
-    'country': sqlalchemy.types.String(length=10), 
-    'beer_style': sqlalchemy.types.String(length=300), 
-    'availability': sqlalchemy.types.String(length=100), 
-    'abv': sqlalchemy.types.INTEGER, 
-    'brewery_name': sqlalchemy.types.String(length=100), 
-    'city': sqlalchemy.types.String(length=50), 
-    'types': sqlalchemy.types.String(length=50), 
-    'Category': sqlalchemy.types.String(length=50), 
-    'ABV (min)': sqlalchemy.types.INTEGER, 
-    'ABV (max)': sqlalchemy.types.INTEGER, 
-    'ABV (avg)': sqlalchemy.types.INTEGER, 
-    'IBU (min)': sqlalchemy.types.INTEGER, 
-    'IBU (max)': sqlalchemy.types.INTEGER, 
-    'IBU (avg)': sqlalchemy.types.INTEGER, 
-    'SRM Range': sqlalchemy.types.String(length=50), 
-    'Glassware': sqlalchemy.types.String(length=50), 
+    'beer_id': sqlalchemy.types.INTEGER,
+    'score': sqlalchemy.types.FLOAT,
+    'beer_name': sqlalchemy.types.String(length=300),
+    'brewery_id': sqlalchemy.types.INTEGER,
+    'state': sqlalchemy.types.String(length=50),
+    'country': sqlalchemy.types.String(length=10),
+    'beer_style': sqlalchemy.types.String(length=300),
+    'availability': sqlalchemy.types.String(length=100),
+    'abv': sqlalchemy.types.FLOAT,
+    'brewery_name': sqlalchemy.types.String(length=100),
+    'city': sqlalchemy.types.String(length=50),
+    'types': sqlalchemy.types.String(length=50),
+    'Category': sqlalchemy.types.String(length=50),
+    'ABV (min)': sqlalchemy.types.FLOAT,
+    'ABV (max)': sqlalchemy.types.FLOAT,
+    'ABV (avg)': sqlalchemy.types.FLOAT,
+    'IBU (min)': sqlalchemy.types.INTEGER,
+    'IBU (max)': sqlalchemy.types.INTEGER,
+    'IBU (avg)': sqlalchemy.types.FLOAT,
+    'SRM Range': sqlalchemy.types.String(length=50),
+    'Glassware': sqlalchemy.types.String(length=50),
     'Description': sqlalchemy.types.String(length=500)
     })
-
