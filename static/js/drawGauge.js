d433f71fb8655728c570b0c28b51cd1e516fca2d
sheetalbongale <57145360+sheetalbongale@users.noreply.github.com>
2020-04-02 06:24:37 -0500

    Add app.js - Fails to show the Gauge or the description

--- static/js/drawGauge.js
+++ static/js/drawGauge.js
@@ -1,6 +1,6 @@
 
-function drawGauge(data) {
-	let degree = parseInt(data.ABV_avg) * (180/11);
+function drawGaugeABV(data) {
+	let degree = parseFloat(data.ABV_avg) * (180/11);
 
 	let level = degree;
 
@@ -34,9 +34,79 @@ function drawGauge(data) {
 	textfont:{
 	  size : 30,
 	  },
-	marker: {colors:['rgba(217, 230, 242, .5)','rgba(141, 180, 216 ,.5)',
-						'rgba(65, 130, 190, .5)', 'rgba(39, 78, 114, .5)', 
-	                       'rgba(13, 26, 38, .5)','rgba(255, 255, 255, 0)'
+	marker: {colors:['rgb(227, 153, 7)','rgb(224, 164, 45)',
+	'rgb(227, 179, 84)', 'rgb(227, 202, 152)', 
+	   'rgb(232, 222, 202)','rgba(255, 255, 255, 0)'
+]},
+	hoverinfo: 'text',
+	hole: .5,
+	type: 'pie',
+	showlegend: false
+	}];
+
+	let layout = {
+	shapes:[{
+	    type: 'path',
+	    path: path,
+	    fillcolor: '850000',
+	    line: {
+	      color: '850000'
+	    }
+	  }],
+
+	title: `<b> ABV % for </b> <br>${data.Style}<br>`,
+	height: 550,
+	width: 550,
+	xaxis: {zeroline:false, showticklabels:false,
+	           showgrid: false, range: [-1, 1]},
+	yaxis: {zeroline:false, showticklabels:false,
+	           showgrid: false, range: [-1, 1]},
+	plot_bgcolor: 'rgba(0, 0, 0, 0)',
+	paper_bgcolor: 'rgba(0, 0, 0, 0)',
+	};
+
+	Plotly.newPlot('gauge_abv', trace, layout, {responsive: true});
+
+}
+
+function drawGaugeIBU(data) {
+	let degree = parseFloat(data.IBU_avg) * (180/11);
+
+	let level = degree;
+
+	// Trig to calc meter point
+	let degrees = 180 - level,
+	   radius = .5;
+	let radians = degrees * Math.PI / 180;
+	let x = radius * Math.cos(radians);
+	let y = radius * Math.sin(radians);
+
+	// Path: may have to change to create a better triangle
+	let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+	   pathX = String(x),
+	   space = ' ',
+	   pathY = String(y),
+	   pathEnd = ' Z';
+	let path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+	let trace = [{ type: 'scatter',
+	 x: [0], y:[0],
+	  marker: {size: 28, color:'850000'},
+	  showlegend: false,
+	  name: 'IBU %',
+	  text: data.IBU_avg,
+	  hoverinfo: 'text+name'},
+	{ values: [50/5, 50/5, 50/5, 50/5, 50/5, 50],
+	rotation: 90,
+	text: [ '\u{1F929}', '\u{1F603}', '\u{1F642}','\u{1F610}','\u{1F641}',''],
+	textinfo: 'text',
+	textposition:'inside',
+	textfont:{
+	  size : 30,
+	  },
+	marker: {colors:['rgb(77, 77, 0)','rgb(153, 153, 0)',
+	'rgb(204, 204, 0)', 'rgb(255, 255, 51)', 
+	   'rgb(255, 255, 128)','rgba(255, 255, 255, 0)'
 	                ]},
 	hoverinfo: 'text',
 	hole: .5,
@@ -54,7 +124,7 @@ function drawGauge(data) {
 	    }
 	  }],
 
-	title: `<b> ABV % for </b> <br>${data.Category}<br>`,
+	title: `<b> IBU % for </b> <br>${data.Style}<br>`,
 	height: 550,
 	width: 550,
 	xaxis: {zeroline:false, showticklabels:false,
@@ -65,6 +135,78 @@ function drawGauge(data) {
 	paper_bgcolor: 'rgba(0, 0, 0, 0)',
 	};
 
-	Plotly.newPlot('gauge_abv', trace, layout, {responsive: true});
+	Plotly.newPlot('gauge_ibu', trace, layout, {responsive: true});
 
-	}
+}
+
+function drawGaugeSRM(data) {
+	let degree = parseFloat(data.SRM_avg) * (180/11);
+
+	let level = degree;
+
+	// Trig to calc meter point
+	let degrees = 180 - level,
+	   radius = .5;
+	let radians = degrees * Math.PI / 180;
+	let x = radius * Math.cos(radians);
+	let y = radius * Math.sin(radians);
+
+	// Path: may have to change to create a better triangle
+	let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+	   pathX = String(x),
+	   space = ' ',
+	   pathY = String(y),
+	   pathEnd = ' Z';
+	let path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+	let trace = [{ type: 'scatter',
+	 x: [0], y:[0],
+	  marker: {size: 28, color:'850000'},
+	  showlegend: false,
+	  name: 'SRM %',
+	  text: data.SRM_avg,
+	  hoverinfo: 'text+name'},
+	{ values: [50/5, 50/5, 50/5, 50/5, 50/5, 50],
+	rotation: 90,
+	text: ['Black', 'Dark Brown','Brown','Amber','Pale','Very Light'],
+	textinfo: 'text',
+	textposition:'inside',
+	textfont:{
+	  size : 10,
+	  },
+	marker: {colors:['rgb(115, 77, 1)', 'rgb(166, 114, 10)', 'rgb(224, 193, 56)', 'rgb(255, 229, 117)', 'rgb(255, 243, 191)','rgba(255, 255, 255, 0)'
+					]},
+	labels: ['Black', 'Dark Brown','Brown','Amber','Pale','Very Light'],
+	hoverinfo: data.SRM_avg,
+	hole: .5,
+	type: 'pie',
+	showlegend: false
+	}];
+
+	let layout = {
+	shapes:[{
+	    type: 'path',
+	    path: path,
+	    fillcolor: 'rgba(44, 160, 101, 0.5)',
+	    line: {
+	      color: '850000'
+	    }
+	  }],
+
+	title: `<b> SRM % for </b> <br>${data.Style}<br>`,
+	height: 550,
+	width: 550,
+	xaxis: {zeroline:false, showticklabels:false,
+	           showgrid: false, range: [-1, 1]},
+	yaxis: {zeroline:false, showticklabels:false,
+	           showgrid: false, range: [-1, 1]},
+	plot_bgcolor: 'rgba(0, 0, 0, 0)',
+	paper_bgcolor: 'rgba(0, 0, 0, 0)',
+	};
+
+	Plotly.newPlot('gauge_srm', trace, layout, {responsive: true});
+
+}
+
+
+
